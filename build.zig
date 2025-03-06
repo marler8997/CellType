8836a2e3d793bb8062890af071845b41c5b3f26b
Jonathan Marler <johnnymarler@gmail.com>
2025-03-05 21:57:43 -0700

    zig update 0.14.0

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.Build) void {
         const exe = b.addExecutable(.{
             .name = "genglyphs",
             .root_source_file = b.path("codegen/genglyphs.zig"),
-            .target = b.host,
+            .target = b.graph.host,
         });
         const run = b.addRunArtifact(exe);
         run.addFileArg(b.path("glyphs"));
